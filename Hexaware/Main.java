d5f9fabed6ba304c2ee636a0517684f461ae0130
guruchara <gurucharanchouahn7@gmail.com>
2023-02-06 13:50:08 +0530

    add

---
+++ Hexaware/Main.java
@@ -0,0 +1,14 @@
+
+
+// count frequeny of each character from String 
+
+// like  
+
+//  String s='abaccdda'
+
+//  output :  a-> 3 
+//         b->1
+//         c->2
+//         d->2
+
+// By using HashMap Easily we can solve it 
